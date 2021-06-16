ef884be957d3f01630ad6d33f0ed6bbe1c56c923
Phil Brockman <phil.brockman@gmail.com>
2021-06-16 02:44:08 -0400

    QOL css update

--- app/http/web/in-browser-daw/src/DAW/WorkArea.js
+++ app/http/web/in-browser-daw/src/DAW/WorkArea.js
@@ -26,15 +26,14 @@ const DAWcell = ({content, additionalClasses, fixedWidth=false}) => {
 const BoxColumn = ({header, numKeys}) => {
   return <div className="column">
       <DAWcell content={header} additionalClasses={["header"]}/>
-      {   Array.from(Array(numKeys).keys()).map(box => <DAWcell 
-                                                            key={box}
-                                                            />)  }
   </div>
 }
 
 
 export const WorkArea = ({bpm, totalLengthInSeconds}) => {
   // const midi= tracksToJSON(tracks)
+
+  const unitCell = useSelector(getUnitCell)
   if(totalLengthInSeconds <= 0) return "Select tracks"
 
   const reposition = (n, nodeRef) => {
@@ -48,7 +47,15 @@ export const WorkArea = ({bpm, totalLengthInSeconds}) => {
 
   let pianoKeys = filteredKeys.map((item, idx) => {
     let c = keymaps[item].includes("#") ? "accidental" : "natural";
-    return <DAWcell fixedWidth key={idx} additionalClasses={[c].join(" ")} content={keymaps[item]} />
+    return <DAWcell fixedWidth key={idx} additionalClasses={[c]} content={keymaps[item]} />
+  })
+
+  let horizontalDividers = Array.from(Array(filteredKeys.length+1).keys()).map((item, idx) => {
+    console.log('object', idx*unitCell.height)
+    return <div className="horizontal-division" 
+              style={{top: `${(idx+1)*unitCell.height-1}px`, width: unitCell.oneBeatWidth*numBeats, left: 50}}
+              key={idx}
+              />
   })
 
   let body = boxes.map((box) => {
@@ -60,6 +67,7 @@ export const WorkArea = ({bpm, totalLengthInSeconds}) => {
   {/* <button onClick={() => playMidi(midi)}>Will this play anthynig???</button> */}
       <div className="daw">
         <Subdivisions numCells={numBeats} />
+        {horizontalDividers}
         <DAWBackground body={body} pianoKeys={pianoKeys} />
         <Notes bpm={bpm} />
         <Playhead reposition={reposition}/>
@@ -71,7 +79,7 @@ const Subdivisions = ({numCells}) => {
   const unitCell = useSelector(getUnitCell)
   let offset = 50
   console.log("data", numCells,unitCell.subdivisions)
-  return Array.from(Array(numCells*unitCell.subdivisions).keys()).map (k => {
+  return Array.from(Array(numCells*unitCell.subdivisions+1).keys()).map (k => {
           const divider = 
               <div className="subdivision" 
                 style={{left: `${offset}px`}}
@@ -82,6 +90,8 @@ const Subdivisions = ({numCells}) => {
     }
   )
 }
+
+
 
 const DAWBackground = ({pianoKeys, body})=> {
     console.log('repainting daw background')
